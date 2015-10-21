913c2fcf7b35230db26b173649518c4fe0f02e3f
eterlight <qige2013419@163.com>
2015-10-21 18:40:07 +0800

    Demo新增

---
+++ app.js
@@ -0,0 +1,66 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+//var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
+var flash = require('connect-flash');
+var partials = require('express-partials');
+var util = require('util');
+var app = express();
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(partials());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.configure(function(){
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'ejs');
+	app.use(flash());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret:settings.cookieSecret,
+		store:new MongoStore({
+			db:settings.db
+		})
+	}));
+	//app.use(express.router(routes));
+	app.use(function(req,res,next){
+		res.locals.user=req.session.user;
+      	res.locals.error=req.flash('error').length?req.flash('error'):null;
+      	res.locals.success=req.flash('success').length?req.flash('success'):null;
+		next();
+	});
+
+	app.use(app.router);
+
+	app.use(express.static(__dirname+'/public'));
+});
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+routes(app);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
